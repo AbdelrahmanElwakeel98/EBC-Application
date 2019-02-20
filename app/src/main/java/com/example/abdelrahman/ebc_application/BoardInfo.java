af4d64f4559dc0b16776e6ff31188366ded506e8
abdelrahman <abdelrahman.alsayed.ahmed@gmail.com>
2019-02-20 09:39:37 +0200

    Board page

--- app/src/main/java/com/example/abdelrahman/ebc_application/BoardInfo.java
+++ app/src/main/java/com/example/abdelrahman/ebc_application/BoardInfo.java
@@ -1,4 +1,26 @@
 package com.example.abdelrahman.ebc_application;
 
 public class BoardInfo {
+    private String imgUrl;
+    private String name;
+    private String title;
+
+    public  BoardInfo(String imgUrl, String name, String title){
+        this.imgUrl = imgUrl;
+        this.name = name;
+        this.title = title;
+    }
+
+    public String getImgUrl(){
+        return imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
 }
